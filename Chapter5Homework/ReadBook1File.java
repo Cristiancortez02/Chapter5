10ea3c87686470a9f7f92a64b0376c09d5bf38b8
Cristiancortez02 <111086962+Cristiancortez02@users.noreply.github.com>
2022-10-28 18:55:25 -0400

    Update ReadBook1File.java

--- Chapter5Homework/ReadBook1File.java
+++ Chapter5Homework/ReadBook1File.java
@@ -13,27 +13,45 @@ import java.io.*;
 import java.util.*;
 
 public class ReadBook1File {
-    public static void main(String[] args) throws IOException {
-        int sum = 0;
+    public static void main(String[] args) {
         Scanner inputFile = null;
+        int sum = 0;
+        int max = 0;
+        int min = 0;
+        double average = 0;
+        String regex = ",";
 
         //try/catch for file not found exception
         try {
-            File myFile = new File("Chapter5Homework/Book1.csv");
-            inputFile = new Scanner(new File("Chapter5Homework/Book1.csv"));
-            while (inputFile.hasNextLine()){
-                String numString = inputFile.nextLine();
+            inputFile = new Scanner(new File("src/Chapter5Homework/Book1.csv"));
+            inputFile.useDelimiter(regex);
+            while (inputFile.hasNext()) {
+                String numString = inputFile.next();
                 int num = Integer.parseInt(numString);
                 sum = sum + num;
-                System.out.println(numString);
+
+                if (sum > max) {
+                    max = num;
+                } else if (sum < max){
+                    min = num;
+                } else if (sum == 55){
+                    average = sum / 10;
+                }
+                System.out.print(numString);
             }
-        } catch (Exception e) {
+        } catch (IOException e) {
             e.printStackTrace();
             System.out.println("Can not open or read file.");
         } finally {
             inputFile.close();
         }
+        System.out.println();
         System.out.println("The sum of the numbers is: " + sum);
+        System.out.println("The Lowest Number is: " + min);
+        System.out.println("The Highest number is: " + max);
+        System.out.println("The average of the numbers is: " + average);
+
+
     }
-    }
+}
 
